b40774c568f2b88af26e62689b6a74f447983a3d
StijnMaris <stijn.maris@me.com>
2019-03-18 19:18:24 +0100

    added rendercomponent

--- Minigin/BaseComponent.cpp
+++ Minigin/BaseComponent.cpp
@@ -1,2 +1,6 @@
 #include "MiniginPCH.h"
 #include "BaseComponent.h"
+
+dae::BaseComponent::BaseComponent(std::string name) :m_Name(name)
+{
+}
--- Minigin/BaseComponent.h
+++ Minigin/BaseComponent.h
@@ -4,12 +4,12 @@ namespace dae {
 	class BaseComponent
 	{
 	public:
-		BaseComponent() = default;
+		BaseComponent(std::string name);
 		virtual ~BaseComponent() = default;
 
 		virtual void Update() = 0;
 
 	private:
-		std::string mName{};
+		std::string m_Name{};
 	};
 }
